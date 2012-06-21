1659e97a87c03fe505dc3c89cb05e93d1b9183dc
nikolay.dachin <nikolay.dachin@SOFWKS0023.sof.gameloft.org>
2012-06-21 12:49:00 +0300

    * Commit Test

---
+++ TestClass.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TestClass
+ *
+ * @author nikolay.dachin
+ */
+class TestClass
+{
+    //put your code here
+}
+
+?>
