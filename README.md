c71e3d3d921a1b6b4a693b93d62eeed691a7aa11
I&He <az@azijeon.com>
2012-06-21 13:01:38 +0300

    * Commit Test

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ azijeon-test
 ============
 
 This is test repostitory 
+
+And this is a test change.
---
+++ TestClass_2.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TestClass_2
+ *
+ * @author nikolay.dachin
+ */
+class TestClass_2
+{
+    //put your code here
+}
+
+?>
