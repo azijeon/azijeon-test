d6e247b4f86f546b640eca95e8e99c614d42a325
I&He <az@azijeon.com>
2012-06-21 14:14:02 +0300

    * Commit Test

---
+++ Classes/HelloNurse.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of HelloNurse
+ *
+ * @author nikolay.dachin
+ */
+class HelloNurse {
+    //put your code here
+}
+?>
